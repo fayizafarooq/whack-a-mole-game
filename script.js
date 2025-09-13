24831df943d2c25a75dbb12a6eb068c47964fd80
fayizafarooq <fayizafavaz@gmail.com>
2025-09-13 16:21:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ function startGame() {
 function peep(){ 
   const hole=randomHole(); 
   hole.classList.add("mole") 
-  const time=Math.random() * 500 + 300; 
+  const time=Math.random() * 600 + 400; 
   setTimeout( ()=>{ 
     hole.classList.remove("mole"); 
     if(!timesUp) peep(); 
